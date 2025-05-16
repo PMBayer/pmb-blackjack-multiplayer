c0d7698de15d54eede14e77e9dc5d35fce8f72d0
PMBayer <pascal.bayer1999@gmail.com>
2025-05-16 11:37:16 +0200

    [MPBJ-003] Added InfoPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { CssBaseline } from "@mui/material";
-import Navbar from "./components/NavBar";
+import { Container, CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 import GameBoard from "./components/GameBoard";
+import Navbar from "./components/NavBar";
+import InfoPage from "./pages/InfoPage";
 
 function App() {
   return (
     <>
       <CssBaseline />
       <Navbar />
-      <GameBoard />
+      <Container maxWidth="lg" sx={{ mt: 4 }}>
+        <Routes>
+          <Route path="/" element={<GameBoard />} />
+          <Route path="/info" element={<InfoPage />} />
+        </Routes>
+      </Container>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const theme = createTheme({
@@ -18,9 +19,11 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
